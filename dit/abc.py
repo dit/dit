0dd896bb8f830c6f37e7c1658ca1dfde2589818a
Ryan James <ryangregoryjames@gmail.com>
2013-10-02 13:31:54 -0600

    add dit.abc

---
+++ dit/abc.py
@@ -0,0 +1,10 @@
+"""
+Import several functions as shorthand.
+"""
+
+from dit import Distribution as D
+
+from dit.algorithms import (entropy as H,
+                            mutual information as I,
+                            jensen_shannon_divergence as JSD
+                           )
