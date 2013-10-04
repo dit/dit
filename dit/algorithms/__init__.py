62841bdef117c61c9c902d4a9b3de98a2444fa6e
Ryan James <ryangregoryjames@gmail.com>
2013-10-04 12:50:58 -0600

    add tests for coinformation

--- dit/algorithms/__init__.py
+++ dit/algorithms/__init__.py
@@ -1,3 +1,4 @@
 from .shannon import entropy, conditional_entropy, mutual_information
+from .coinformation import coinformation
 from .jsd import jensen_shannon_divergence
 from .lattice import insert_join, insert_meet
---
+++ dit/algorithms/tests/test_coi.py
@@ -0,0 +1,47 @@
+from __future__ import division
+ 
+from nose.tools import *
+ 
+from dit import Distribution as D
+from dit.algorithms import coinformation as I
+ 
+def test_coi1():
+    outcomes = ['000', '011', '101', '110']
+    pmf = [1/4] * 4
+    d = D(pmf, outcomes)
+    assert_almost_equal(I(d), -1.0)
+
+def test_coi2():
+    outcomes = ['0000', '0011', '0101', '0110',
+                '1001', '1010', '1100', '1111']
+    pmf = [1/8] * 8
+    d = D(pmf, outcomes)
+    assert_almost_equal(I(d), 1.0)
+ 
+def test_coi3():
+    outcomes = ['0000', '0011', '0101', '0110',
+                '1001', '1010', '1100', '1111']
+    pmf = [1/8] * 8
+    d = D(pmf, outcomes)
+    assert_almost_equal(I(d, [[0],[1],[2]]), 0.0)
+ 
+def test_coi4():
+    outcomes = ['0000', '0011', '0101', '0110',
+                '1001', '1010', '1100', '1111']
+    pmf = [1/8] * 8
+    d = D(pmf, outcomes)
+    assert_almost_equal(I(d, [[0],[1],[2]], [3]), -1.0)
+ 
+def test_coi5():
+    outcomes = ['0000', '0011', '0101', '0110',
+                '1001', '1010', '1100', '1111']
+    pmf = [1/8] * 8
+    d = D(pmf, outcomes)
+    assert_almost_equal(I(d, [[0],[1]], [2,3]), 1.0)
+ 
+def test_coi6():
+    outcomes = ['0000', '0011', '0101', '0110',
+                '1001', '1010', '1100', '1111']
+    pmf = [1/8] * 8
+    d = D(pmf, outcomes)
+    assert_almost_equal(I(d, [[0]], [1,2,3]), 0.0)
