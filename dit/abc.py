8c17338574183df90716e4ec974cd92cfb6dd107
Ryan James <ryangregoryjames@gmail.com>
2013-10-05 12:28:23 -0600

    add the total correlation

--- dit/abc.py
+++ dit/abc.py
@@ -6,5 +6,6 @@ from dit import Distribution as D
 
 from dit.algorithms import (entropy as H,
                             mutual information as I,
+                            total_correlation as T,
                             jensen_shannon_divergence as JSD
                            )
