1f180e11749c7494b86b3ddce69709129442cf1f
Ryan James <ryangregoryjames@gmail.com>
2013-10-05 18:44:22 -0600

    switch to using coinformation for I

--- dit/abc.py
+++ dit/abc.py
@@ -5,7 +5,7 @@ Import several functions as shorthand.
 from dit import Distribution as D
 
 from dit.algorithms import (entropy as H,
-                            mutual information as I,
+                            coinformation as I,
                             total_correlation as T,
                             jensen_shannon_divergence as JSD
                            )
