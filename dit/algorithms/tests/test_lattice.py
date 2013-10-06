ecc31a5538d711faded1d3f5e77f2521eee61ca5
Ryan James <ryangregoryjames@gmail.com>
2013-10-06 17:20:25 -0600

    add a couple lattice tests

--- dit/algorithms/tests/test_lattice.py
+++ dit/algorithms/tests/test_lattice.py
@@ -1,7 +1,11 @@
+from __future__ import division
+
+from iterutils import powerset
 
 from nose.tools import *
 
-from dit.algorithms.lattice import sigma_algebra_sort
+from dit import Distribution
+from dit.algorithms.lattice import *
 
 def test_sigalg_sort():
 	sigalg = frozenset([
@@ -12,3 +16,19 @@ def test_sigalg_sort():
 	])
 	sigalg_ = [(), (1,), (2,), (1,2)]
 	assert_equal( sigalg_, sigma_algebra_sort(sigalg) )
+
+def test_join_sigalg():
+    outcomes = ['00', '01', '10', '11']
+    pmf = [1/4]*4
+    d = Distribution(outcomes, pmf)
+    sigalg = frozenset([ frozenset(_) for _ in powerset(outcomes) ])
+    join = join_sigalg(d, [[0],[1]])
+    assert_equal(sigalg, join)
+
+def test_meet_sigalg():
+    outcomes = ['00', '01', '10', '11']
+    pmf = [1/4]*4
+    d = Distribution(outcomes, pmf)
+    sigalg = frozenset([ frozenset([]), frozenset(outcomes) ])
+    meet = meet_sigalg(d, [[0],[1]])
+    assert_equal(sigalg, meet)
