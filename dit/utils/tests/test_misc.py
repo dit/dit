9c4c33461ddb2d621c2b393a779aa67fea3bef84
Ryan James <ryangregoryjames@gmail.com>
2013-10-18 23:24:29 -0600

    fix test for py2.6, again.

--- dit/utils/tests/test_misc.py
+++ dit/utils/tests/test_misc.py
@@ -4,8 +4,11 @@ from dit.utils.misc import flatten, is_string_like
 
 def test_flatten1():
     x = [[[0], 1, 2], 3, [4]]
+    fx = list(flatten(x))
     y = [0, 1, 2, 3, 4]
-    assert_list_equal(list(flatten(x)), y)
+    assert_equal(len(fx), len(y))
+    for i, j in zip(fx, y):
+        assert_equal(i, j)
 
 def test_is_string_like1():
     ys = ['', 'hi', "pants", '"test"', u'pants', r'pants']
