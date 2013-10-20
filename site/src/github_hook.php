61667089f561d7e8e5345ed3bf4cd744de7249ee
chebee7i <chebee7i@gmail.com>
2013-10-20 05:34:20 -0500

    Missing semicolon.

--- site/src/github_hook.php
+++ site/src/github_hook.php
@@ -6,7 +6,7 @@
 <pre>
     <?php
     # We do not background the call since we need it to finish before building.
-    shell_exec('git pull')
+    shell_exec('git pull >> ../build.log 2>&1');
     shell_exec('python ../dit/site/build.py ../public_html 5 >> ../build.log 2>&1 &');
     ?>
     Done.
