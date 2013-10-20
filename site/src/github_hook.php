3f42b7aec4b05202dd3fb0b8ac4105ee3a800e8d
chebee7i <chebee7i@gmail.com>
2013-10-20 05:39:33 -0500

    Make sure php is in git repo.

--- site/src/github_hook.php
+++ site/src/github_hook.php
@@ -6,8 +6,9 @@
 <pre>
     <?php
     # We do not background the call since we need it to finish before building.
+    chdir('../dit');
     shell_exec('git pull >> ../build.log 2>&1');
-    shell_exec('python ../dit/site/build.py ../public_html 5 >> ../build.log 2>&1 &');
+    shell_exec('python site/build.py ../public_html 5 >> ../build.log 2>&1 &');
     ?>
     Done.
 </pre>
