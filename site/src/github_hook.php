2b015a541bdace369046761c97ec890014129086
chebee7i <chebee7i@gmail.com>
2013-10-20 05:44:26 -0500

    Have php print date before git pull.

--- site/src/github_hook.php
+++ site/src/github_hook.php
@@ -7,10 +7,11 @@
     <?php
     # We do not background the call since we need it to finish before building.
     chdir('../dit');
+    shell_exec('date >> ../build.log 2>&1');
     shell_exec('git pull >> ../build.log 2>&1');
     shell_exec('python site/build.py ../public_html 5 >> ../build.log 2>&1 &');
     ?>
-    Done.
+Build request received.
 </pre>
 </body>
 </html>
