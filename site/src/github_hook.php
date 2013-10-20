a18ccd68eeb96250cab6e010bcb83bd2b68e4067
chebee7i <chebee7i@gmail.com>
2013-10-20 06:21:09 -0500

    Add some comments about editing github_hook.php.

--- site/src/github_hook.php
+++ site/src/github_hook.php
@@ -5,7 +5,10 @@
 <body>
 <pre>
 <?php
-    # We do not background the call since we need it to finish before building.
+    // We do not background the call since we need it to finish before the
+    // new webpage is built. Current directory is: /var/www/dit.io/public_html
+    // If this file is messed up somehow, you will likely need to manually
+    // update the git repository on the server and then run build.py.
     chdir('../dit');
     shell_exec('date >> ../build.log 2>&1');
     shell_exec('git pull >> ../build.log 2>&1');
