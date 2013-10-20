45921f6e5ed563d0a704b2553959bd983f764ded
chebee7i <chebee7i@gmail.com>
2013-10-20 04:40:10 -0500

    Attempt to build website on each commit.

---
+++ site/build.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: ascii -*-
+
+"""
+This script can be used to build the website.
+
+It is also run on each commit to github.
+
+Example:  ./build ../public_html
+
+"""
+
+import os
+import shutil
+import subprocess
+import sys
+import time
+
+BUILD_DIR = 'build'
+
+def get_build_dir():
+    try:
+        build_dir = sys.argv[1]
+    except IndexError:
+        build_dir = BUILD_DIR
+
+    basedir = os.path.abspath(os.path.curdir)
+    build_dir = os.path.join(basedir, build_dir)
+    return build_dir
+
+def build(dest):
+    source = os.path.split(os.path.abspath(__file__))[0]
+    source = os.path.join(source, 'src')
+
+    # We aren't doing anything fancy yet.
+    shutil.copytree(source, dest)
+
+def main():
+    try:
+        min_delay = int(sys.argv[2]) * 60
+    except:
+        min_delay = 0
+
+    # Build only if enough time has passed.
+    build_dir = get_build_dir()
+    if os.path.exists(build_dir):
+        elapsed = time.time() - os.path.getmtime(build_dir)
+        if elapsed < min_delay:
+            print "Not enough time has elapsed since last build."
+            sys.exit(0)
+        else:
+            # Delete it all!
+            if os.path.islink(build_dir):
+                os.unlink(build_dir)
+            else:
+                shutil.rmtree(build_dir)
+    elif os.path.islink(build_dir):
+        # Then its a bad symlink.
+        os.unlink(build_dir)
+
+    subprocess.call(['git', 'pull'])
+    build(build_dir)
+    subprocess.call(['touch', build_dir])
+    print "Done."
+
+if __name__ == '__main__':
+    main()
---
+++ site/src/github_hook.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>dit</title>
+</head>
+<body>
+<pre>
+    <?php
+        shell_exec('python ../dit/site/build.py ../public_html 5 > ../build.log 2>../build.log &');
+    ?>
+    Done.
+</pre>
+</body>
+</html>
