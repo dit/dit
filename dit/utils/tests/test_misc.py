a05f21503a9aaa2b461bf1d908d7913c4dcff52d
Ryan James <ryangregoryjames@gmail.com>
2013-10-25 11:01:03 -0600

    a couple more tests

--- dit/utils/tests/test_misc.py
+++ dit/utils/tests/test_misc.py
@@ -1,6 +1,7 @@
 from nose.tools import *
 
-from dit.utils.misc import flatten, is_string_like
+from dit.utils.misc import flatten, is_string_like, partitions, partitions2, \
+                           ordered_partitions
 from six import u
 
 def test_flatten1():
@@ -18,3 +19,15 @@ def test_is_string_like1():
         assert_true(is_string_like(y))
     for n in ns:
         assert_false(is_string_like(n))
+
+def test_partitions1():
+    bells = [1, 2, 5, 15, 52]
+    for i, b in enumerate(bells):
+        parts = list(partitions(range(i+1)))
+        assert_equal(len(parts), b)
+
+def test_ordered_partitions1():
+    obells = [1, 1, 3, 13, 75]
+    for i, b in enumerate(obells):
+        oparts = list(ordered_partitions(range(i)))
+        assert_equal(len(oparts), b)
