d045bed08dcbc1ed27cc7c2cbe1c48353a184998
Ryan James <ryangregoryjames@gmail.com>
2013-10-31 14:54:50 -0600

    update copyright to dit contributors

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'dit'
-copyright = u'2013, Chris Ellison and Ryan James'
+copyright = u'2013, dit contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
