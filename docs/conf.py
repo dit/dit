ada43382b90f488b50d49bf286152629ad138336
Ryan James <ryangregoryjames@gmail.com>
2013-10-31 03:10:40 -0600

    add numpydoc to the sphinx extensions

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,13 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.coverage', 'sphinx.ext.mathjax', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.intersphinx',
+              'sphinx.ext.coverage',
+              'sphinx.ext.mathjax',
+              'sphinx.ext.viewcode',
+              'numpydoc',
+             ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
