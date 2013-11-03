643b32f161fa8447721c4ebbd782c953b917e212
Ryan James <ryangregoryjames@gmail.com>
2013-11-03 13:47:22 -0700

    fix conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -99,17 +99,18 @@ pygments_style = 'sphinx'
 
 # -- Options for HTML output ---------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-html_theme = 'better'
-
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ["_themes"]
+from better import better_theme_path
+html_theme_path = ["_themes", better_theme_path]
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+html_theme = 'better'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
