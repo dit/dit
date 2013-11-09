9fc715ce93a9c2c11f2bc9fe9d2e58ff7c43f21c
chebee7i <chebee7i@gmail.com>
2013-11-09 16:24:31 -0600

    Build with sphinx_rtd_theme when not on RTD.

--- docs/conf.py
+++ docs/conf.py
@@ -105,17 +105,18 @@ todo_include_todos = not on_rtd
 
 # -- Options for HTML output ---------------------------------------------------
 
+# Add any paths that contain custom themes here, relative to this directory.
+html_theme_path = ["_themes"]
+
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'default'
+if not on_rtd:
+    html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
-
-# Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = ["_themes"]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
