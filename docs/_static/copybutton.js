54897d43059154844206f80f0a41d6745e172895
chebee7i <chebee7i@gmail.com>
2013-11-10 21:24:21 -0600

    Update copybutton so that it recognizes all python-like code-blocks.

--- docs/_static/copybutton.js
+++ docs/_static/copybutton.js
@@ -1,9 +1,35 @@
+/* Add a [>>>] button on the top-right corner of code samples to hide
+ * the >>> and ... prompts and the output and thus make the code
+ * copyable.
+ */
+
 $(document).ready(function() {
-    /* Add a [>>>] button on the top-right corner of code samples to hide
-     * the >>> and ... prompts and the output and thus make the code
-     * copyable. */
+
+    /* Note, the outer div of code blocks has a class like 'highlight-python'
+     * when the specified language was 'python'. It turns out that there are
+     * many ways to specify the language, and they can correspond to different
+     * lexers. As of now, one cannot determine from the HTML what lexer was
+     * used to parse the source code. As an example, Sphinx will autodetect
+     * if the code block represents a python console, then the "pycon" lexer
+     * is used instead of the "python" lexer, but the class on the outer div
+     * might still be 'highlight-python'.
+     */
     var div = $('.highlight-python .highlight,' +
-                '.highlight-python3 .highlight')
+                '.highlight-py .highlight,' +
+                '.highlight-sage .highlight,' +
+                '.highlight-python3 .highlight,' +
+                '.highlight-py3 .highlight,' +
+                '.highlight-pycon .highlight,' +
+                '.highlight-pycon3 .highlight,' +
+                /*
+                    Note, 'pycon3' is not an alias of any Python lexer found in
+                    :mod:`pygements.lexers.agile`.  However, it is provided by
+                    :mod:`sphinx.highlighting`.
+                */
+                '.highlight-pytb .highlight,' +
+                '.highlight-py3tb .highlight,' +
+                '.highlight-ipython .highlight')
+
     var pre = div.find('pre');
 
     // get the styles from the current theme
@@ -16,8 +42,9 @@ $(document).ready(function() {
     var button_styles = {
         'cursor':'pointer', 'position': 'absolute', 'top': '0', 'right': '0',
         'border-color': border_color, 'border-style': border_style,
-        'border-width': border_width, 'color': border_color, 'text-size': '75%',
-        'font-family': 'monospace', 'padding-left': '0.2em', 'padding-right': '0.2em',
+        'border-width': border_width, 'color': border_color,
+        'text-size': '75%', 'font-family': 'monospace',
+        'padding-left': '0.2em',  'padding-right': '0.2em',
         'border-radius': '0 3px 0 0'
     }
 
@@ -25,16 +52,20 @@ $(document).ready(function() {
     div.each(function(index) {
         var jthis = $(this);
         if (jthis.find('.gp').length > 0) {
+            // TODO: use something more generic that makes sense for IPython.
             var button = $('<span class="copybutton">&gt;&gt;&gt;</span>');
             button.css(button_styles)
             button.attr('title', hide_text);
             jthis.prepend(button);
         }
         // tracebacks (.gt) contain bare text elements that need to be
-        // wrapped in a span to work with .nextUntil() (see later)
-        jthis.find('pre:has(.gt)').contents().filter(function() {
-            return ((this.nodeType == 3) && (this.data.trim().length > 0));
-        }).wrap('<span>');
+        // wrapped in a span to work with .nextUntil() (see toggle code)
+        jthis.find('pre:has(.gt)').contents()
+            .filter(function() {
+                return ((this.nodeType == 3) && (this.data.trim().length > 0));
+            })
+                .wrap('<span>')
+            .end();
     });
 
     // define the behavior of the button when it's clicked
