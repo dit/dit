fcbb58dfad3d188b074b87f7362000c226c1c423
Ryan James <ryangregoryjames@gmail.com>
2013-11-20 20:24:07 -0700

    add another tse test

--- dit/algorithms/tests/test_tse.py
+++ dit/algorithms/tests/test_tse.py
@@ -4,6 +4,7 @@ from nose.tools import assert_almost_equal
 
 from iterutils import powerset
 
+from dit import Distribution as D
 from dit.algorithms import binding_information as B, tse_complexity as TSE
 from dit.example_dists import n_mod_m
 from dit.math.misc import combinations as nCk
@@ -15,4 +16,10 @@ def test_tse1():
         indices = [ [k] for k in range(i) ]
         tse = TSE(d)
         x = 1/2 * sum(B(d, rv)/nCk(i, len(rv)) for rv in powerset(indices))
-        assert_almost_equal(tse, x)
+        yield assert_almost_equal, tse, x
+
+def test_tse2():
+    for n in xrange(2, 7):
+        d = D(['0'*n, '1'*n], [1/2, 1/2])
+        tse = TSE(d)
+        yield assert_almost_equal, tse, (n-1)/2
