dit/dit

commit 6d10da1b4f949f74fba5344302299d71a355eaf6

d._meta was incorrectly shared among instances. Fix issue with condition_on when dealing with dense distributions.

dit/npdist.py
@@ -323,11 +323,7 @@ class Distribution(ScalarDistribution):
     ## Unadvertised attributes
     _sample_space = None
     _mask = None
-    _meta = {
-        'is_joint': True,
-        'is_numerical': True,
-        'is_sparse': None,
-    }
+    _meta = None
     _outcome_class = None
     _outcome_ctor = None
     _outcomes_index = None
@@ -419,6 +415,11 @@ class Distribution(ScalarDistribution):
         # We do this because we want to init the prng AND ignore everything
         # that ScalarDistribution does.
         super(ScalarDistribution, self).__init__(prng)
+
+        # Set *instance* attributes
+        self._meta['is_joint'] = True
+        self._meta['is_numerical'] = True
+        self._meta['is_sparse'] = None
 
         # Do any checks/conversions necessary to get the parameters.
         outcomes, pmf = self._init(outcomes, pmf, base)
@@ -866,7 +867,13 @@ class Distribution(ScalarDistribution):
             cindexes = [mapping[idx] for idx in cindexes]
             indexes = [mapping[idx] for idx in indexes]
         else:
-            d = self
+            # Make a copy so we don't have to worry about changing the input
+            # distribution when we make it sparse.
+            d = self.copy()
+
+        # It's just easier to not worry about conditioning on zero probs.
+        sparse = d.is_sparse()
+        d.make_sparse()
 
         cdist = d.marginal(cindexes, rv_names='indices')
         dist = d.marginal(indexes, rv_names='indices')
@@ -878,12 +885,12 @@ class Distribution(ScalarDistribution):
         ctor = d._outcome_ctor
 
         # A list of indexes of conditioned outcomes for each joint outcome.
-        # These are the indexes of w in P(w) for each ws in P(ws).
+        # These are the indexes of w in the pmf of P(w) for each ws in P(ws).
         cidx = cdist._outcomes_index
         coutcomes = [cidx[ctor([o[i] for i in cindexes])] for o in d.outcomes]
 
         # A list of indexes of outcomes for each joint outcome.
-        # These are the indexes of s in P(s) for each ws in P(ws).
+        # These are the indexes of s in the pmf of P(s) for each ws in P(ws).
         idx = dist._outcomes_index
         outcomes = [idx[ctor([o[i] for i in indexes])] for o in d.outcomes]
 
@@ -895,7 +902,7 @@ class Distribution(ScalarDistribution):
         pmfs.fill(ops.zero)
         for i, (coutcome, outcome) in enumerate(zip(coutcomes, outcomes)):
             pmfs[coutcome, outcome] = probs[i]
-        dists = [Distribution(dist.outcomes, pmfs[i],
+        dists = [Distribution(dist.outcomes, pmfs[i], sparse=sparse,
                  base=base, sample_space=sample_space, validate=False)
                  for i in range(pmfs.shape[0])]
         for dist in dists:

dit/distribution.py
@@ -273,12 +273,7 @@ class BaseDistribution(object):
         could be a log probability or a linear probability.
 
     """
-    # Subclasses should update these meta attributes *before* calling the base
-    # distribution's __init__ function.
-    _meta = {
-        'is_joint': None,
-        'is_numerical': None,
-    }
+    _meta = None
 
     # These should be set in the subclass's init function.
     outcomes = None
@@ -298,6 +293,12 @@ class BaseDistribution(object):
             import dit.math
             prng = dit.math.prng
         self.prng = prng
+
+        self._meta = {
+            'is_joint': None,
+            'is_numerical': None,
+        }
+
 
     def __contains__(self, outcome):
         raise NotImplementedError

dit/npscalardist.py
@@ -231,11 +231,7 @@ class ScalarDistribution(BaseDistribution):
 
     _sample_space = None
     _outcomes_index = None
-    _meta = {
-        'is_joint': False,
-        'is_numerical': True,
-        'is_sparse': None
-    }
+    _meta = None
 
     alphabet = None
     outcomes = None
@@ -313,6 +309,11 @@ class ScalarDistribution(BaseDistribution):
 
         """
         super(ScalarDistribution, self).__init__(prng)
+
+        # Set *instance* attributes.
+        self._meta['is_joint'] = False
+        self._meta['is_numerical'] = True
+        self._meta['is_sparse'] = None
 
         outcomes, pmf = self._init(outcomes, pmf, base)
 

commit 8bc4b77e3960880c83e93dc9320efafc544ebcd4

Fix issue with copy() not setting _meta.
This fixes a bug introduced in 6d10da1b4f.

dit/inference/pycounts.pyx
@@ -103,7 +103,7 @@ def standardize_data(data, alphabet=None,
     cdef int K = len(alphabet)
     cdef int i, symbol
 
-    # Note this will be slower for an array than for a list since NumPy
+    # Note data[i] will be slower for an array than for a list since NumPy
     # indexing is slower than pure Python indexing.
 
     if K == 1:

dit/npdist.py
@@ -121,6 +121,10 @@ def _make_distribution(outcomes, pmf, base,
     # This sets the prng.
     super(ScalarDistribution, d).__init__(prng)
 
+    d._meta['is_joint'] = True
+    d._meta['is_numerical'] = True
+    d._meta['is_sparse'] = None
+
     if base is None:
         # Assume default base.
         base = ditParams['base']