25760d30c26f45b652204d7c0bcafa1fe0b4d34f
chebee7i <chebee7i@gmail.com>
2014-08-07 14:27:08 -0500

    Update install_requires to include prettytable.

--- setup.py
+++ setup.py
@@ -179,6 +179,7 @@ def main():
     install_requires = [
         'numpy >= 1.6',
         'iterutils >= 0.1.6',
+        'prettytable',
         'six >= 1.4.0', # 1.4.0 includes six.moves.range.
     ]
     if sys.version_info[:2] == (2, 6):
