330a8b833022d628f91e7bb499b206d06b3d4abd
Ryan James <ryangregoryjames@gmail.com>
2015-03-07 16:33:26 -0800

    finish docstrings

--- dit/algorithms/minimal_sufficient_statistic.py
+++ dit/algorithms/minimal_sufficient_statistic.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """
+Functions for computing minimal sufficient statistics.
 """
 
 from collections import defaultdict
@@ -16,6 +17,20 @@ __all__ = ['mss_sigalg',
 def partial_match(first, second, places):
     """
     Returns whether `second` is a marginal outcome at `places` of `first`.
+
+    Parameters
+    ----------
+    first : iterable
+        The un-marginalized outcome.
+    second : iterable
+        The smaller, marginalized outcome.
+    places : list
+        The locations of `second` in `first`.
+
+    Returns
+    -------
+    match : bool
+        Whether `first` and `second` match or not.
     """
     return tuple([first[i] for i in places]) == tuple(second)
 
@@ -26,12 +41,34 @@ def mss_sigalg(dist, rvs, about=None, rv_mode=None):
 
     Parameters
     ----------
+    dist : Distribution
+        The distribution which defines the base sigma-algebra.
+    rvs : list
+        A list of random variables to be compressed into a minimal sufficient
+        statistic.
+    about : list
+        A list of random variables for which the minimal sufficient static will
+        retain all information about.
+    rv_mode : str, None
+        Specifies how to interpret the elements of `rvs`. Valid options are:
+        {'indices', 'names'}. If equal to 'indices', then the elements of
+        `rvs` are interpreted as random variable indices. If equal to 'names',
+        the the elements are interpreted as random variable names. If `None`,
+        then the value of `dist._rv_mode` is consulted.
 
     Returns
     -------
+    mss_sa : frozenset of frozensets
+        The induced sigma-algebra of the minimal sufficient statistic.
 
     Examples
     --------
+    >>> d = Xor()
+    >>> mss_sigalg(d, [0], [1, 2])
+    frozenset({frozenset(),
+               frozenset({'000', '011'}),
+               frozenset({'101', '110'}),
+               frozenset({'000', '011', '101', '110'})})
     """
     mapping = parse_rvs(dist, about)[1]
 
@@ -39,29 +76,66 @@ def mss_sigalg(dist, rvs, about=None, rv_mode=None):
 
     md, cds = dist.condition_on(rvs=rvs, crvs=about, rv_mode=rv_mode)
 
-    for outcome, cd in zip(md.outcomes, cds):
-        matches = [ o for o in dist.outcomes if partial_match(o, outcome, mapping) ]
+    for marg, cd in zip(md.outcomes, cds):
+        matches = [o for o in dist.outcomes if partial_match(o, marg, mapping)]
         for c in partition.keys():
             if c.is_approx_equal(cd):
-                eq[c].extend(matches)
+                partition[c].extend(matches)
                 break
         else:
-            eq[cd].extend(matches)
+            partition[cd].extend(matches)
 
-    mss_sa = sigma_algebra(map(frozenset, eq.values()))
+    mss_sa = sigma_algebra(map(frozenset, partition.values()))
 
     return mss_sa
 
-def insert_mss(dist, idx=-1, rvs, about=None, rv_mode=None):
+def insert_mss(dist, idx, rvs, about=None, rv_mode=None):
     """
+    Inserts the minimal sufficient statistic of `rvs` about `about` into `dist`
+    at index `idx`.
+
     Parameters
     ----------
+    dist : Distribution
+        The distribution which defines the base sigma-algebra.
+    idx : int
+        The location in the distribution to insert the minimal sufficient
+        statistic.
+    rvs : list
+        A list of random variables to be compressed into a minimal sufficient
+        statistic.
+    about : list
+        A list of random variables for which the minimal sufficient static will
+        retain all information about.
+    rv_mode : str, None
+        Specifies how to interpret the elements of `rvs`. Valid options are:
+        {'indices', 'names'}. If equal to 'indices', then the elements of
+        `rvs` are interpreted as random variable indices. If equal to 'names',
+        the the elements are interpreted as random variable names. If `None`,
+        then the value of `dist._rv_mode` is consulted.
 
     Returns
     -------
+    d : Distribution
+        The distribution `dist` modified to contain the minimal sufficient
+        statistic.
 
     Examples
     --------
+    >>> d = Xor()
+    >>> print(insert_mss(d, -1, [0], [1, 2]))
+    Class:          Distribution
+    Alphabet:       ('0', '1') for all rvs
+    Base:           linear
+    Outcome Class:  str
+    Outcome Length: 4
+    RV Names:       None
+
+    x      p(x)
+    0000   0.25
+    0110   0.25
+    1011   0.25
+    1101   0.25
     """
     mss_sa = mss_sigalg(dist, rvs, about, rv_mode)
     new_dist = insert_rv(dist, idx, mss_sa)
@@ -71,13 +145,42 @@ def mss(dist, rvs, about=None, rv_mode=None, int_outcomes=True):
     """
     Parameters
     ----------
+    dist : Distribution
+        The distribution which defines the base sigma-algebra.
+    rvs : list
+        A list of random variables to be compressed into a minimal sufficient
+        statistic.
+    about : list
+        A list of random variables for which the minimal sufficient static will
+        retain all information about.
+    rv_mode : str, None
+        Specifies how to interpret the elements of `rvs`. Valid options are:
+        {'indices', 'names'}. If equal to 'indices', then the elements of `rvs`
+        are interpreted as random variable indices. If equal to 'names', the the
+        elements are interpreted as random variable names. If `None`, then the
+        value of `dist._rv_mode` is consulted.
+    int_outcomes : bool
+        If `True`, then the outcomes of the minimal sufficient statistic are
+        relabeled as integers instead of as the atoms of the induced
+        sigma-algebra.
 
     Returns
     -------
+    d : Distribution
+        The distribution of the minimal sufficient statistic.
 
     Examples
     --------
+    >>> d = Xor()
+    >>> print(mss(d, [0], [1, 2]))
+    Class:    ScalarDistribution
+    Alphabet: (0, 1)
+    Base:     linear
+
+    x   p(x)
+    0   0.5
+    1   0.5
     """
-    mss_sa = mss_sigalg
+    mss_sa = mss_sigalg(dist, rvs, about, rv_mode)
     d = dist_from_induced_sigalg(dist, mss_sa, int_outcomes)
     return d
