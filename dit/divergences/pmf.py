3850ccaecb70e235a32c5afc23acd01c0ad3d932
chebee7i <chebee7i@gmail.com>
2015-03-12 12:32:37 -0500

    Hide kl_nonmerge from dit.divergences

--- dit/divergences/pmf.py
+++ dit/divergences/pmf.py
@@ -6,7 +6,7 @@ from .jensen_shannon_divergence import (
     jensen_shannon_divergence_pmf as jensen_shannon_divergence
 )
 
-from .kl_nonmerge import (
+from ._kl_nonmerge import (
     cross_entropy_pmf as cross_entropy,
     relative_entropy_pmf as relative_entropy
 )
