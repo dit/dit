98c019c2328f4524019bf3a8cfb2e40842e2c7c7
chebee7i <chebee7i@gmail.com>
2015-03-11 19:14:17 -0500

    Add an example script for maxent.

---
+++ examples/example_maxent.py
@@ -0,0 +1,69 @@
+from __future__ import print_function
+
+import dit
+import numpy as np
+
+# The functions will import this for you...just make sure you have it.
+import cvxopt
+
+def print_output(d, maxent_dists):
+    # Calculate the entropy for each.
+    entropies = np.asarray(map(dit.shannon.entropy, maxent_dists))
+    print()
+    print("Entropies:")
+    print(entropies)
+
+    # Differences are what we learn at each step.
+    netinfo = -1 * np.diff(entropies)
+    print()
+    print("Network Informations:")
+    print(netinfo)
+
+    # Total correlation is what is learned at ith from (i-1)th starting at i=2.
+    total_corr = netinfo[1:].sum()
+    total_corr_true = dit.multivariate.total_correlation(d)
+    print()
+    print("Total correlation: {0} (numerically)\t {1} (true)".format(total_corr, total_corr_true))
+    print()
+
+def example_A():
+    """
+    Calculate network information using marginal maxentropy.
+
+    """
+    d = dit.example_dists.Xor()
+
+    # Calculate marginal maximum entropy distributions up to order 3.
+    maxent_dists = dit.algorithms.marginal_maxent_dists(d, 3)
+
+    print_output(d, maxent_dists)
+
+    return maxent_dists
+
+def example_B():
+    """
+    Calculate network information using moment-based maxentropy.
+
+    """
+    d = dit.example_dists.Xor()
+
+    # Calculate moment maximum entropy distributions up to order 3.
+    mapping = [-1, 1]
+    maxent_dists = dit.algorithms.moment_maxent_dists(d, mapping, 3, with_replacement=False)
+
+    print_output(d, maxent_dists)
+
+    return maxent_dists
+
+def example_C():
+    # Giant bit, perfect correlation.
+
+    # Note, doesn't converge if we do this with n=4. e.g.: '1111', '0000'. Lol!
+    outcomes = ['111', '000']
+    d = dit.Distribution(outcomes, [.5, .5])
+    maxent_dists = dit.algorithms.marginal_maxent_dists(d)
+    print_output(d, maxent_dists)
+
+if __name__ == '__main__':
+    example_A()
+
