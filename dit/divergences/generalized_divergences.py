321c69dd59591b262655b912a33359ac3be9576f
Marc Harper <marc.harper@gmail.com>
2015-06-03 17:36:37 -0700

    Generalized divergences -- Renyi, Tsallis, Hellinger, and alpha, with many tests

---
+++ dit/divergences/generalized_divergences.py
@@ -0,0 +1,273 @@
+"""
+The Kullback-Leibler divergence.
+"""
+
+import numpy as np
+
+from .cross_entropy import get_pmfs_like
+from ..helpers import normalize_rvs
+from ..utils import flatten
+
+from .kullback_leibler_divergence import kullback_leibler_divergence
+
+__all__ = ('double_power_sum',
+           'hellinger_sum',
+           'alpha_divergence',
+           'hellinger_divergence',
+           'renyi_divergence',
+           'tsallis_divergence',
+          )
+
+### References for Divergence Formulas ###
+## http://arxiv.org/pdf/1105.3259v1.pdf
+## http://mitran-lab.amath.unc.edu:8082/subversion/grants/Proposals/2013/DOE-DataCentric/biblio/LieseVajdaDivergencesInforTheory.pdf
+
+def double_power_sum(dist1, dist2, exp1, exp2, rvs=None, crvs=None, rv_mode=None):
+    """A common generalization of the sums needed to compute the Hellinger and alpha divergences below.
+
+    Parameters
+    ----------
+    dist1 : Distribution
+        The first distribution in the Kullback-Leibler divergence.
+    dist2 : Distribution
+        The second distribution in the Kullback-Leibler divergence.
+    rvs : list, None
+        The indexes of the random variable used to calculate the
+        Kullback-Leibler divergence between. If None, then the Kullback-Leibler
+        divergence is calculated over all random variables.
+    rv_mode : str, None
+        Specifies how to interpret `rvs` and `crvs`. Valid options are:
+        {'indices', 'names'}. If equal to 'indices', then the elements of
+        `crvs` and `rvs` are interpreted as random variable indices. If equal
+        to 'names', the the elements are interpreted as random variable names.
+        If `None`, then the value of `dist._rv_mode` is consulted, which
+        defaults to 'indices'.
+
+    Returns
+    -------
+    dkl : float
+        The specified sum between `dist1` and `dist2`.
+
+    Raises
+    ------
+    ditException
+        Raised if either `dist1` or `dist2` doesn't have `rvs` or, if `rvs` is
+        None, if `dist2` has an outcome length different than `dist1`.
+
+    """
+
+    rvs, crvs, rv_mode = normalize_rvs(dist1, rvs, crvs, rv_mode)
+    rvs, crvs = list(flatten(rvs)), list(flatten(crvs))
+    normalize_rvs(dist2, rvs, crvs, rv_mode)
+
+    p1s, q1s = get_pmfs_like(dist1, dist2, rvs+crvs, rv_mode)
+    div = np.nansum(np.power(p1s, exp1) * np.power(q1s, exp2))
+
+    if crvs:
+        p2s, q2s = get_pmfs_like(dist1, dist2, crvs, rv_mode)
+        div = np.nansum(np.power(p2s, exp1) * np.power(q2s, exp2))
+
+    return div
+
+def hellinger_sum(dist1, dist2, alpha=1., rvs=None, crvs=None, rv_mode=None):
+    """
+    The Hellinger sum/integral of `dist1` and `dist2`, used to define other divergences.
+
+    Parameters
+    ----------
+    dist1 : Distribution
+        The first distribution in the Kullback-Leibler divergence.
+    dist2 : Distribution
+        The second distribution in the Kullback-Leibler divergence.
+    rvs : list, None
+        The indexes of the random variable used to calculate the
+        Kullback-Leibler divergence between. If None, then the Kullback-Leibler
+        divergence is calculated over all random variables.
+    rv_mode : str, None
+        Specifies how to interpret `rvs` and `crvs`. Valid options are:
+        {'indices', 'names'}. If equal to 'indices', then the elements of
+        `crvs` and `rvs` are interpreted as random variable indices. If equal
+        to 'names', the the elements are interpreted as random variable names.
+        If `None`, then the value of `dist._rv_mode` is consulted, which
+        defaults to 'indices'.
+
+    Returns
+    -------
+    dkl : float
+        The Hellinger sum between `dist1` and `dist2`.
+
+    Raises
+    ------
+    ditException
+        Raised if either `dist1` or `dist2` doesn't have `rvs` or, if `rvs` is
+        None, if `dist2` has an outcome length different than `dist1`.
+
+    """
+
+    return double_power_sum(dist1, dist2, alpha, 1.-alpha, rvs=rvs, crvs=crvs, rv_mode=rv_mode)
+
+def hellinger_divergence(dist1, dist2, alpha=1., rvs=None, crvs=None, rv_mode=None):
+    # http://mitran-lab.amath.unc.edu:8082/subversion/grants/Proposals/2013/DOE-DataCentric/biblio/LieseVajdaDivergencesInforTheory.pdf
+    """
+    The Hellinger divergence of `dist1` and `dist2`.
+
+    Parameters
+    ----------
+    dist1 : Distribution
+        The first distribution in the Kullback-Leibler divergence.
+    dist2 : Distribution
+        The second distribution in the Kullback-Leibler divergence.
+    rvs : list, None
+        The indexes of the random variable used to calculate the
+        Kullback-Leibler divergence between. If None, then the Kullback-Leibler
+        divergence is calculated over all random variables.
+    rv_mode : str, None
+        Specifies how to interpret `rvs` and `crvs`. Valid options are:
+        {'indices', 'names'}. If equal to 'indices', then the elements of
+        `crvs` and `rvs` are interpreted as random variable indices. If equal
+        to 'names', the the elements are interpreted as random variable names.
+        If `None`, then the value of `dist._rv_mode` is consulted, which
+        defaults to 'indices'.
+
+    Returns
+    -------
+    dkl : float
+        The Hellinger divergence between `dist1` and `dist2`.
+
+    Raises
+    ------
+    ditException
+        Raised if either `dist1` or `dist2` doesn't have `rvs` or, if `rvs` is
+        None, if `dist2` has an outcome length different than `dist1`.
+
+    """
+    
+    if alpha == 1:
+        return kullback_leibler_divergence(dist1, dist2, rvs=rvs, crvs=crvs, rv_mode=rv_mode)
+    s = hellinger_sum(dist1, dist2, rvs=rvs, alpha=alpha, crvs=crvs, rv_mode=rv_mode)
+    return (s-1.)/(alpha-1.)
+
+def tsallis_divergence(dist1, dist2, alpha=1., rvs=None, crvs=None, rv_mode=None):
+    """
+    The Tsallis divergence of `dist1` and `dist2`.
+
+    Parameters
+    ----------
+    dist1 : Distribution
+        The first distribution in the Kullback-Leibler divergence.
+    dist2 : Distribution
+        The second distribution in the Kullback-Leibler divergence.
+    rvs : list, None
+        The indexes of the random variable used to calculate the
+        Kullback-Leibler divergence between. If None, then the Kullback-Leibler
+        divergence is calculated over all random variables.
+    rv_mode : str, None
+        Specifies how to interpret `rvs` and `crvs`. Valid options are:
+        {'indices', 'names'}. If equal to 'indices', then the elements of
+        `crvs` and `rvs` are interpreted as random variable indices. If equal
+        to 'names', the the elements are interpreted as random variable names.
+        If `None`, then the value of `dist._rv_mode` is consulted, which
+        defaults to 'indices'.
+
+    Returns
+    -------
+    dkl : float
+        The Tsallis divergence between `dist1` and `dist2`.
+
+    Raises
+    ------
+    ditException
+        Raised if either `dist1` or `dist2` doesn't have `rvs` or, if `rvs` is
+        None, if `dist2` has an outcome length different than `dist1`.
+
+    """
+
+    # D_T = (D_alpha -1) / (alpha-1)
+    if alpha == 1:
+        return kullback_leibler_divergence(dist1, dist2, rvs=rvs, crvs=crvs, rv_mode=rv_mode)
+    return np.log2(hellinger_sum(dist1, dist2, alpha=alpha, rvs=rvs, crvs=crvs, rv_mode=rv_mode)) / (alpha - 1.)
+
+
+def renyi_divergence(dist1, dist2, alpha=1., rvs=None, crvs=None, rv_mode=None):
+    """
+    The Renyi divergence of `dist1` and `dist2`.
+
+    Parameters
+    ----------
+    dist1 : Distribution
+        The first distribution in the Kullback-Leibler divergence.
+    dist2 : Distribution
+        The second distribution in the Kullback-Leibler divergence.
+    rvs : list, None
+        The indexes of the random variable used to calculate the
+        Kullback-Leibler divergence between. If None, then the Kullback-Leibler
+        divergence is calculated over all random variables.
+    rv_mode : str, None
+        Specifies how to interpret `rvs` and `crvs`. Valid options are:
+        {'indices', 'names'}. If equal to 'indices', then the elements of
+        `crvs` and `rvs` are interpreted as random variable indices. If equal
+        to 'names', the the elements are interpreted as random variable names.
+        If `None`, then the value of `dist._rv_mode` is consulted, which
+        defaults to 'indices'.
+
+    Returns
+    -------
+    dkl : float
+        The Renyi divergence between `dist1` and `dist2`.
+
+    Raises
+    ------
+    ditException
+        Raised if either `dist1` or `dist2` doesn't have `rvs` or, if `rvs` is
+        None, if `dist2` has an outcome length different than `dist1`.
+
+    """
+
+    # D_R = log D_alpha / (alpha-1)
+    if alpha == 1:
+        return kullback_leibler_divergence(dist1, dist2, rvs=rvs, crvs=crvs, rv_mode=rv_mode)
+    return (hellinger_sum(dist1, dist2, rvs=rvs, alpha=alpha, crvs=crvs, rv_mode=rv_mode) - 1.) /(alpha - 1.)
+
+def alpha_divergence(dist1, dist2, alpha=1., rvs=None, crvs=None, rv_mode=None):
+    """
+    The alpha divergence of `dist1` and `dist2`, as used in Information Geometry. Note there is more than one inequivalent definition of "alpha divergence" in the literature, this one comes from http://en.wikipedia.org/wiki/Information_geometry .
+
+    Parameters
+    ----------
+    dist1 : Distribution
+        The first distribution in the Kullback-Leibler divergence.
+    dist2 : Distribution
+        The second distribution in the Kullback-Leibler divergence.
+    rvs : list, None
+        The indexes of the random variable used to calculate the
+        Kullback-Leibler divergence between. If None, then the Kullback-Leibler
+        divergence is calculated over all random variables.
+    rv_mode : str, None
+        Specifies how to interpret `rvs` and `crvs`. Valid options are:
+        {'indices', 'names'}. If equal to 'indices', then the elements of
+        `crvs` and `rvs` are interpreted as random variable indices. If equal
+        to 'names', the the elements are interpreted as random variable names.
+        If `None`, then the value of `dist._rv_mode` is consulted, which
+        defaults to 'indices'.
+
+    Returns
+    -------
+    dkl : float
+        The alpha divergence between `dist1` and `dist2`.
+
+    Raises
+    ------
+    ditException
+        Raised if either `dist1` or `dist2` doesn't have `rvs` or, if `rvs` is
+        None, if `dist2` has an outcome length different than `dist1`.
+
+    """
+
+    if alpha == 1:
+        return kullback_leibler_divergence(dist1, dist2, rvs=rvs, crvs=crvs, rv_mode=rv_mode)
+    if alpha == -1:
+        return kullback_leibler_divergence(dist2, dist1, rvs=rvs, crvs=crvs, rv_mode=rv_mode)
+    s = double_power_sum(dist1, dist2, (1.-alpha)/2, (1.+alpha)/2, rvs=rvs, crvs=crvs, rv_mode=rv_mode)
+    return 4*(1.-s)/(1.-alpha*alpha)
+
+
---
+++ dit/divergences/tests/test_generalized_divergences.py
@@ -0,0 +1,105 @@
+"""
+Tests for dit.divergences.kullback_leiber_divergence.
+"""
+
+from __future__ import division
+
+from nose.tools import assert_almost_equal, assert_not_equal, assert_raises
+
+import numpy as np
+
+from dit import Distribution
+from dit.exceptions import ditException
+from dit.divergences import kullback_leibler_divergence, alpha_divergence, renyi_divergence, tsallis_divergence, hellinger_divergence, hellinger_sum
+
+def get_dists_1():
+    """
+    Construct several example distributions.
+    """
+    d1 = Distribution(['0', '1'], [1/2, 1/2])
+    d2 = Distribution(['0', '2'], [1/2, 1/2])
+    d3 = Distribution(['0', '1', '2'], [1/3, 1/3, 1/3])
+    d4 = Distribution(['00', '11'], [2/5, 3/5])
+    d5 = Distribution(['00', '11'], [1/2, 1/2])
+    return d1, d2, d3, d4, d5
+
+def get_dists_2():
+    """
+    Construct several example distributions.
+    """
+    d1 = Distribution(['0', '1'], [1/2, 1/2])
+    d2 = Distribution(['0', '1'], [1/3, 2/3])
+    d3 = Distribution(['0', '1'], [2/5, 3/5])
+    return d1, d2, d3
+
+def get_dists_3():
+    """
+    Construct several example distributions.
+    """
+    d1 = Distribution(['0', '1', '2'], [1/5, 2/5, 2/5])
+    d2 = Distribution(['0', '1', '2'], [1/4, 1/2, 1/4])
+    d3 = Distribution(['0', '1', '2'], [1/3, 1/3, 1/3])
+    d4 = Distribution(['0', '1', '2'], [1/6, 2/6, 3/6])
+    return d1, d2, d3, d4
+
+def test_positive_definite():
+    """
+    Tests that divergences are zero when the input distributions are the same, and that the Hellinger sum is equal to 1.
+    """
+    alphas = [0, 1, 2, 0.5]
+    divergences = [alpha_divergence, renyi_divergence, tsallis_divergence, hellinger_divergence]
+    for dist in get_dists_1():
+        for alpha in alphas:
+            for divergence in divergences:
+                assert_almost_equal(divergence(dist, dist, alpha), 0)
+            assert_almost_equal(hellinger_sum(dist, dist, alpha), 1)
+
+def test_alpha_symmetry():
+    """
+    Tests the alpha -> -alpha symmetry for the alpha divergence, and a similar symmetry for the Hellinger divergence.
+    """
+    alphas = [-1, 0, 0.5, 1, 2]
+    test_dists = [get_dists_2(), get_dists_3()]
+    for alpha in alphas:
+        for dists in test_dists:
+            for dist1 in dists:
+                for dist2 in dists:
+                    assert_almost_equal(alpha_divergence(dist1, dist2, alpha), alpha_divergence(dist2, dist1, -alpha))
+                    assert_almost_equal((1.-alpha)*hellinger_divergence(dist1, dist2, alpha), alpha*hellinger_divergence(dist2, dist1, 1.-alpha))
+
+def test_divergences_to_kl():
+    """
+    Tests that the generalized divergences properly fall back to KL for the appropriate values of alpha, and not otherwise.
+    """
+
+    test_dists = [get_dists_2(), get_dists_3()]
+    for dists in test_dists:
+        for dist1 in dists:
+            for dist2 in dists:
+                assert_almost_equal(alpha_divergence(dist1, dist2, alpha=-1), kullback_leibler_divergence(dist2, dist1))
+                if dist1 != dist2:
+                    assert_not_equal(alpha_divergence(dist1, dist2, alpha=0), kullback_leibler_divergence(dist2, dist1))
+                for divergence in [renyi_divergence, tsallis_divergence, alpha_divergence, hellinger_divergence]:
+                    assert_almost_equal(divergence(dist1, dist2, alpha=1), kullback_leibler_divergence(dist1, dist2))
+                    if dist1 != dist2:
+                        assert_not_equal(alpha_divergence(dist1, dist2, alpha=0), kullback_leibler_divergence(dist2, dist1))
+                        assert_not_equal(alpha_divergence(dist1, dist2, alpha=2), kullback_leibler_divergence(dist2, dist1))
+
+def test_exceptions():
+    """
+    Test that when p has outcomes that q doesn't have, that we raise an exception.
+    """
+    d1, d2, d3, d4, d5 = get_dists_1()
+    tests = [[d4, d1, None, None],
+             [d4, d2, None, None],
+             [d4, d3, None, None],
+             [d1, d2, [0, 1], None],
+             [d3, d4, [1], None],
+             [d5, d1, [0], [1]],
+             [d4, d3, [1], [0]]]
+    divergences = [alpha_divergence, renyi_divergence, tsallis_divergence, hellinger_divergence]
+    alphas = [0, 1, 2, 0.5]
+    for first, second, rvs, crvs in tests:
+        for divergence in divergences:
+            for alpha in alphas:
+                yield assert_raises, ditException, divergence, first, second, alpha, rvs, crvs
