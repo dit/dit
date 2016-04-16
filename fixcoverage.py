7b631d6851adbee9365616994754dcab962d5a09
Ryan James <ryangregoryjames@gmail.com>
2016-04-16 00:06:58 -0700

    go back to a more meaningful variable name

--- fixcoverage.py
+++ fixcoverage.py
@@ -5,19 +5,19 @@ import sys
 def main(argv):
     source = argv[1]
     dest = argv[2]
-    cd = coverage.CoverageData()
-    cd.read_file('.coverage')
+    coverage_data = coverage.CoverageData()
+    coverage_data.read_file('.coverage')
 
     # Prefilter to filenames in dit
-    filenames = [filename for filename in cd._lines.keys()
+    filenames = [filename for filename in coverage_data._lines.keys()
                  if 'dit' in filename]
 
     for filename in filenames:
         new_filename = re.sub(source, dest, filename)
         if new_filename != filename:
-            cd._lines[new_filename] = cd._lines.pop(filename)
+            coverage_data._lines[new_filename] = coverage_data._lines.pop(filename)
 
-    cd.write_file('.coverage')
+    coverage_data.write_file('.coverage')
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
