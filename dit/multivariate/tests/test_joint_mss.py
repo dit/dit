a61d4122c0f50325b43d8ea74bcae007d4cc24bc
Ryan James <ryangregoryjames@gmail.com>
2016-04-21 18:01:21 -0700

    add a new test

--- dit/multivariate/tests/test_joint_mss.py
+++ dit/multivariate/tests/test_joint_mss.py
@@ -30,3 +30,26 @@ def test_M1():
     pmf = [1/16]*16
     d = Distribution(outcomes, pmf)
     assert_almost_equal(M(d), 2.0)
+
+def test_M2():
+    """ Test M with rv names """
+    outcomes = ['000',
+                'a00',
+                '00c',
+                'a0c',
+                '011',
+                'a11',
+                '101',
+                'b01',
+                '01d',
+                'a1d',
+                '10d',
+                'b0d',
+                '110',
+                'b10',
+                '11c',
+                'b1c',]
+    pmf = [1/16]*16
+    d = Distribution(outcomes, pmf)
+    d.set_rv_names('XYZ')
+    assert_almost_equal(M(d), 2.0)
