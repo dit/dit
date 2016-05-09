38eee33e5a766eb2a3cf9bd06e3bd910ac8ee0a6
Ryan James <ryangregoryjames@gmail.com>
2016-05-09 14:51:18 -0700

    work on kripp decomp

--- dit/profiles/information_partitions.py
+++ dit/profiles/information_partitions.py
@@ -58,6 +58,10 @@ def constraint_lattice(elements):
     def less_than(sss1, sss2):
         return all(any(ss1 <= ss2 for ss2 in sss2) for ss1 in sss1)
 
+    def normalize(sss):
+        return tuple(sorted(tuple( tuple(sorted(ss)) for ss in sss ),
+                            key=lambda s: (-len(s), s)))
+
     elements = set(elements)
 
     ps = (frozenset(ss) for ss in powerset(elements) if len(ss) > 0)
@@ -70,7 +74,8 @@ def constraint_lattice(elements):
 
     for a, b in order:
         if not any(((a, c) in order) and ((c, b) in order) for c in pps):
-            lattice.add_edge(tuple(map(tuple, b)), tuple(map(tuple, a)))
+
+            lattice.add_edge(normalize(b), normalize(a))
 
     return lattice
 
@@ -259,13 +264,13 @@ class ExtropyPartition(BaseInformationPartition):
         return 'X'
 
 
-class KrippendorfPartition(object):
+class KrippendorffPartition(object):
     """
     Construct a decomposition of all the dependencies in a given joint
     distribution.
     """
 
-    def __init__(self, dist, tol=1e-3):
+    def __init__(self, dist, tol=1e-4):
         """
         Construct a Krippendorff-type partition of the information contained in
         `dist`.
@@ -280,24 +285,15 @@ class KrippendorfPartition(object):
         self._partition()
 
     @staticmethod
-    def _stringify(rvs, crvs):
+    def _stringify(dependency):
         """
-        Construct a string representation of a measure, e.g. I[X:Y|Z]
+        Construct a string representation of a dependency, e.g. ABC:AD:BD
 
         Parameters
         ----------
-        rvs : list
-            The random variable(s) for the measure.
-        crvs : list
-            The random variable(s) that the measure is conditioned on.
+        dependency : tuple of tuples
         """
-        rvs = [','.join(str(_) for _ in rv) for rv in rvs]
-        crvs = [str(_) for _ in crvs]
-        a = ':'.join(rvs)
-        b = ','.join(crvs)
-        symbol = 'H' if len(rvs) == 1 else 'I'
-        sep = '|' if len(crvs) > 0 else ''
-        s = "{0}[{1}{2}{3}]".format(symbol, a, sep, b)
+        s = ':'.join(''.join(map(str, d)) for d in dependency if len(d) > 1)
         return s
 
     def _partition(self):
@@ -315,49 +311,49 @@ class KrippendorfPartition(object):
         if not rvs:
             rvs = tuple(range(self.dist.outcome_length()))
 
-        lattice = constraint_lattice(rvs)
-        rlattice = lattice.reverse()
-        Hs = {}
-        Is = {}
+        self._lattice = constraint_lattice(rvs)
+        # rlattice = lattice.reverse()
+        # Hs = {}
+        # Is = {}
         atoms = {}
-        new_atoms = {}
+        # new_atoms = {}
 
         # Entropies
-        for node in lattice:
-            Hs[node] = H(maxent_dist(self.dist, node, tol=self._tol))
+        for node in self._lattice:
+            atoms[node] = H(maxent_dist(self.dist, node, tol=self._tol))
 
-        # Subset-sum type thing, basically co-information calculations.
-        for node in lattice:
-            Is[node] = sum((-1)**(len(rv)+1)*Hs[rv] for rv in children(lattice, node))
+        # # Subset-sum type thing, basically co-information calculations.
+        # for node in lattice:
+        #     Is[node] = sum((-1)**(len(rv)+1)*Hs[rv] for rv in children(lattice, node))
+        #
+        # # Mobius inversion of the above, resulting in the Shannon atoms.
+        # for node in topological_sort(lattice)[:-1]:
+        #     kids = islice(children(rlattice, node), 1, None)
+        #     atoms[node] = Is[node] - sum(atoms[child] for child in kids)
+        #
+        # # get the atom indices in proper format
+        # for atom, value in atoms.items():
+        #     a_rvs = tuple((_,) for _ in atom)
+        #     a_crvs = tuple(sorted(set(rvs) - set(atom)))
+        #     new_atoms[(a_rvs, a_crvs)] = value
+        #
+        self.atoms = atoms
 
-        # Mobius inversion of the above, resulting in the Shannon atoms.
-        for node in topological_sort(lattice)[:-1]:
-            kids = islice(children(rlattice, node), 1, None)
-            atoms[node] = Is[node] - sum(atoms[child] for child in kids)
-
-        # get the atom indices in proper format
-        for atom, value in atoms.items():
-            a_rvs = tuple((_,) for _ in atom)
-            a_crvs = tuple(sorted(set(rvs) - set(atom)))
-            new_atoms[(a_rvs, a_crvs)] = value
-
-        self.atoms = new_atoms
-
-    def __getitem__(self, item):
-        """
-        Return the value of any information measure.
-
-        Parameters
-        ----------
-        item : tuple
-            A pair (rvs, crvs).
-        """
-        def is_part(atom, rvs, crvs):
-            lhs = all(any(((_,) in atom[0]) for _ in rv) for rv in rvs)
-            rhs = set(crvs).issubset(atom[1])
-            return lhs and rhs
-
-        return sum(value for atom, value in self.atoms.items() if is_part(atom, *item))
+    # def __getitem__(self, item):
+    #     """
+    #     Return the value of any information measure.
+    #
+    #     Parameters
+    #     ----------
+    #     item : tuple
+    #         A pair (rvs, crvs).
+    #     """
+    #     def is_part(atom, rvs, crvs):
+    #         lhs = all(any(((_,) in atom[0]) for _ in rv) for rv in rvs)
+    #         rhs = set(crvs).issubset(atom[1])
+    #         return lhs and rhs
+    #
+    #     return sum(value for atom, value in self.atoms.items() if is_part(atom, *item))
 
 
     def __str__(self):
@@ -370,31 +366,31 @@ class KrippendorfPartition(object):
         """
         Use PrettyTable to create a nice table.
         """
-        table = PrettyTable(['measure', 'bits'])
+        table = PrettyTable(['dependency', 'bits'])
         ### TODO: add some logic for the format string, so things look nice
         # with arbitrary values
         table.float_format['bits'] = ' 5.{0}'.format(digits)
-        key_function = lambda row: (len(row[0][0]), row[0][0], row[0][1])
+        key_function = lambda row: ([len(d) for d in row[0]], row[0])
         items = self.atoms.items()
-        for (rvs, crvs), value in sorted(items, key=key_function):
+        for dependency, value in reversed(sorted(items, key=key_function)):
             # gets rid of pesky -0.0 display values
             if close(value, 0.0):
                 value = 0.0
-            table.add_row([self._stringify(rvs, crvs), value])
+            table.add_row([self._stringify(dependency), value])
         return table.get_string()
 
-    def get_atoms(self, string=True):
-        """
-        Return all the atoms for the distribution.
-
-        Parameters
-        ----------
-        string : bool
-            If True, return atoms as strings. Otherwise, as a pair of tuples.
-        """
-        if string:
-            f = self._stringify
-        else:
-            f = lambda a, b: (a, b)
-
-        return set([f(rvs, crvs) for rvs, crvs in self.atoms.keys()])
+    # def get_atoms(self, string=True):
+    #     """
+    #     Return all the atoms for the distribution.
+    #
+    #     Parameters
+    #     ----------
+    #     string : bool
+    #         If True, return atoms as strings. Otherwise, as a pair of tuples.
+    #     """
+    #     if string:
+    #         f = self._stringify
+    #     else:
+    #         f = lambda a, b: (a, b)
+    #
+    #     return set([f(rvs, crvs) for rvs, crvs in self.atoms.keys()])
