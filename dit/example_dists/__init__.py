2d57e2c2f45dcc916d15daaef5de0e072c664b8b
Ryan James <ryangregoryjames@gmail.com>
2017-05-24 08:42:25 -0700

    expose giant_bit

--- dit/example_dists/__init__.py
+++ dit/example_dists/__init__.py
@@ -4,6 +4,7 @@ A selection of generic distributions which could be useful.
 
 from .circuits import Unq, Rdn, Xor, And, Or, RdnXor, ImperfectRdn, Subtle
 from .dice import iid_sum, summed_dice
+from .giant_bit import giant_bit
 from .mdbsi import dyadic, triadic
 from .n_mod_m import n_mod_m
 from .numeric import bernoulli, binomial, hypergeometric, uniform
