dfe3586decb3746bca6a30d2d250301651290aac
Ryan James <ryangregoryjames@gmail.com>
2017-11-06 17:23:48 -0800

    fix ci test

--- dit/multivariate/tests/test_common_informations.py
+++ dit/multivariate/tests/test_common_informations.py
@@ -23,8 +23,29 @@ epsilon = 1e-6
 
 @pytest.mark.slow
 @pytest.mark.flaky(reruns=5)
-@given(dist=distributions(size=(2, 3), alphabet=(2, 3)))
+@given(dist=distributions(alphabets=((2, 3),)*2))
 def test_cis1(dist):
+    """
+    Test that the common informations are ordered correctly.
+    """
+    k = K(dist)
+    j = J(dist)
+    b = B(dist)
+    c = C(dist)
+    g = G(dist)
+    f = F(dist)
+    m = M(dist)
+    assert k <= j + epsilon
+    assert j <= b + epsilon
+    assert b <= c + epsilon
+    assert c <= g + epsilon
+    assert g <= f + epsilon
+    assert f <= m + epsilon
+
+@pytest.mark.slow
+@pytest.mark.flaky(reruns=5)
+@given(dist=distributions(alphabets=((2, 3),)*3))
+def test_cis2(dist):
     """
     Test that the common informations are ordered correctly.
     """
