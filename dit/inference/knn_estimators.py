0af0544905f079ada5ea341fbb7e2124be359076
Ryan James <ryangregoryjames@gmail.com>
2017-11-09 11:28:23 -0800

    add a faster, scikit-learn backed ksg function

--- dit/inference/knn_estimators.py
+++ dit/inference/knn_estimators.py
@@ -7,12 +7,22 @@ from __future__ import division
 import numpy as np
 
 from scipy.special import digamma
-from scipy.spatial import cKDTree
 
 from six.moves import zip
 
 from dit.utils import flatten
 
+try:
+    from sklearn.neighbors import KDTree
+    total_correlation_ksg = _total_correlation_ksg_sklearn
+except ImportError:
+    from scipy.spatial import cKDTree
+    total_correlation_ksg = _total_correlation_ksg_scipy
+
+
+__all__ = ['differential_entropy_knn',
+           'total_correlation_ksg',
+          ]
 
 def _fuzz(data, noise):
     """
@@ -73,7 +83,7 @@ def differential_entropy_knn(data, rvs=None, k=4, noise=1e-10):
     return h / np.log(2)
 
 
-def total_correlation_ksg(data, rvs, crvs=None, k=4, noise=1e-10):
+def _total_correlation_ksg_scipy(data, rvs, crvs=None, k=4, noise=1e-10):
     """
     Compute the total correlation from observations. The total correlation is computed between the columns
     specified in `rvs`, given the columns specified in `crvs`. This utilizes the KSG kNN density estimator,
@@ -134,6 +144,77 @@ def total_correlation_ksg(data, rvs, crvs=None, k=4, noise=1e-10):
         tree_crvs = cKDTree(data[:, crvs])
         n_crvs = np.array([len(tree_crvs.query_ball_point(point, epsilon, p=np.inf)) for point, epsilon in
                            zip(data[:, crvs], epsilons)])
+        h_crvs = -digamma(n_crvs).mean()
+    else:
+        h_rvs = [h_rv + digamma_N + d * (log_2 - log_epsilons).mean() for h_rv, d in zip(h_rvs, d_rvs)]
+        h_all += digamma_N + sum(d_rvs) * (log_2 - log_epsilons).mean()
+        h_crvs = 0
+
+    tc = sum([h_rv - h_crvs for h_rv in h_rvs]) - (h_all - h_crvs)
+
+    return tc / log_2
+
+
+def _total_correlation_ksg_sklearn(data, rvs, crvs=None, k=4, noise=1e-10):
+    """
+    Compute the total correlation from observations. The total correlation is computed between the columns
+    specified in `rvs`, given the columns specified in `crvs`. This utilizes the KSG kNN density estimator,
+    and works on discrete, continuous, and mixed data.
+
+    Parameters
+    ----------
+    data : np.array
+        Real valued time series data.
+    rvs : iterable of iterables
+        The columns for which the total correlation is to be computed.
+    crvs : iterable
+        The columns upon which the total correlation should be conditioned.
+    k : int
+        The number of nearest neighbors to use in estimating the local kernel density.
+    noise : float
+        The standard deviation of the normally-distributed noise to add to the data.
+
+    Returns
+    -------
+    tc : float
+        The total correlation of `rvs` given `crvs`.
+
+    Notes
+    -----
+    The total correlation is computed in bits, not nats as most KSG estimators do.
+
+    This implementation uses scikit-learn.
+    """
+    # KSG suggest adding noise (to break symmetries?)
+    data = _fuzz(data, noise)
+
+    if crvs is None:
+        crvs = []
+
+    digamma_N = digamma(len(data))
+    log_2 = np.log(2)
+
+    all_rvs = list(flatten(rvs)) + crvs
+    rvs = [rv + crvs for rv in rvs]
+
+    d_rvs = [len(data[0, rv]) for rv in rvs]
+
+    tree = KDTree(data[:, all_rvs], metric="chebyshev")
+    tree_rvs = [KDTree(data[:, rv], metric="chebyshev") for rv in rvs]
+
+    epsilons = tree.query(data[:, all_rvs], k + 1)[0][:, -1]  # k+1 because of self
+
+    n_rvs = [t.query_radius(data[:, rv], epsilons, count_only=True) for rv, t in zip(rvs, tree_rvs)]
+
+    log_epsilons = np.log(epsilons)
+
+    h_rvs = [-digamma(n_rv).mean() for n_rv, d in zip(n_rvs, d_rvs)]
+
+    h_all = -digamma(k)
+
+    if crvs:
+        tree_crvs = KDTree(data[:, crvs], metric="chebyshev")
+        n_crvs = tree_crvs.query_radius(data[:, crvs], epsilons, count_only=True)
         h_crvs = -digamma(n_crvs).mean()
     else:
         h_rvs = [h_rv + digamma_N + d * (log_2 - log_epsilons).mean() for h_rv, d in zip(h_rvs, d_rvs)]
