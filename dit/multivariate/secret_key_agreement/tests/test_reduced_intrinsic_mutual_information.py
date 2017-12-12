824e875611a51a0044527b8866a9b6ae6adf5839
Ryan James <ryangregoryjames@gmail.com>
2017-12-11 18:14:26 -0800

    fix pytest call

--- dit/multivariate/secret_key_agreement/tests/test_reduced_intrinsic_mutual_information.py
+++ dit/multivariate/secret_key_agreement/tests/test_reduced_intrinsic_mutual_information.py
@@ -8,7 +8,7 @@ from dit.example_dists.intrinsic import *
 from dit.multivariate import reduced_intrinsic_total_correlation
 
 @pytest.skip
-@pytest.mark.parametrize(('dist', val), [(intrinsic_1, 0.0), (intrinsic_2, 1.0), (intrinsic_3, 1.0)])
+@pytest.mark.parametrize(('dist', 'val'), [(intrinsic_1, 0.0), (intrinsic_2, 1.0), (intrinsic_3, 1.0)])
 def test_1(dist, val):
     """
     Test against known values.
