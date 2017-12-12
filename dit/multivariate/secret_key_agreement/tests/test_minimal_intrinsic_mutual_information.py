be485a7de63e7e517f49b6fc8793f87a4d14d9a4
Ryan James <ryangregoryjames@gmail.com>
2017-12-11 18:52:13 -0800

    add more tests

--- dit/multivariate/secret_key_agreement/tests/test_minimal_intrinsic_mutual_information.py
+++ dit/multivariate/secret_key_agreement/tests/test_minimal_intrinsic_mutual_information.py
@@ -5,7 +5,10 @@ Tests for dit.multivariate.secret_key_agreement.minimal_intrinsic_mutual_informa
 import pytest
 
 from dit.example_dists.intrinsic import *
-from dit.multivariate import minimal_intrinsic_total_correlation
+from dit.multivariate import (minimal_intrinsic_total_correlation,
+                              minimal_intrinsic_dual_total_correlation,
+                              minimal_intrinsic_CAEKL_mutual_information,
+                              )
 
 @pytest.mark.parametrize('dist', [intrinsic_1, intrinsic_2, intrinsic_3])
 def test_1(dist):
@@ -13,4 +16,20 @@ def test_1(dist):
     Test against known values.
     """
     mimi = minimal_intrinsic_total_correlation(dist, [[0], [1]], [2], bounds=(3,))
+    assert mimi == pytest.approx(dist.secret_rate, abs=1e-5)
+
+@pytest.mark.parametrize('dist', [intrinsic_1, intrinsic_2, intrinsic_3])
+def test_2(dist):
+    """
+    Test against known values.
+    """
+    mimi = minimal_intrinsic_dual_total_correlation(dist, [[0], [1]], [2], bounds=(3,))
+    assert mimi == pytest.approx(dist.secret_rate, abs=1e-5)
+
+@pytest.mark.parametrize('dist', [intrinsic_1, intrinsic_2, intrinsic_3])
+def test_3(dist):
+    """
+    Test against known values.
+    """
+    mimi = minimal_intrinsic_CAEKL_mutual_information(dist, [[0], [1]], [2], bounds=(3,))
     assert mimi == pytest.approx(dist.secret_rate, abs=1e-5)
--- dit/multivariate/secret_key_agreement/tests/test_reduced_intrinsic_mutual_information.py
+++ dit/multivariate/secret_key_agreement/tests/test_reduced_intrinsic_mutual_information.py
@@ -5,7 +5,10 @@ Tests for dit.multivariate.secret_key_agreement.reduced_intrinsic_mutual_informa
 import pytest
 
 from dit.example_dists.intrinsic import *
-from dit.multivariate import reduced_intrinsic_total_correlation
+from dit.multivariate import (reduced_intrinsic_total_correlation,
+                              reduced_intrinsic_dual_total_correlation,
+                              reduced_intrinsic_CAEKL_mutual_information,
+                              )
 
 @pytest.mark.skip(reason="Calculation of RIMI is prohibitively slow.")
 @pytest.mark.parametrize(('dist', 'val'), [(intrinsic_1, 0.0), (intrinsic_2, 1.0), (intrinsic_3, 1.0)])
@@ -14,4 +17,22 @@ def test_1(dist, val):
     Test against known values.
     """
     rimi = reduced_intrinsic_total_correlation(dist, [[0], [1]], [2])
+    assert rimi == pytest.approx(val, abs=1e-5)
+
+@pytest.mark.skip(reason="Calculation of RIMI is prohibitively slow.")
+@pytest.mark.parametrize(('dist', 'val'), [(intrinsic_1, 0.0), (intrinsic_2, 1.0), (intrinsic_3, 1.0)])
+def test_2(dist, val):
+    """
+    Test against known values.
+    """
+    rimi = reduced_intrinsic_dual_total_correlation(dist, [[0], [1]], [2])
+    assert rimi == pytest.approx(val, abs=1e-5)
+
+@pytest.mark.skip(reason="Calculation of RIMI is prohibitively slow.")
+@pytest.mark.parametrize(('dist', 'val'), [(intrinsic_1, 0.0), (intrinsic_2, 1.0), (intrinsic_3, 1.0)])
+def test_3(dist, val):
+    """
+    Test against known values.
+    """
+    rimi = reduced_intrinsic_CAEKL_mutual_information(dist, [[0], [1]], [2])
     assert rimi == pytest.approx(val, abs=1e-5)
