8d1a09aef25e3d47ee0d9ef26c1b40ab917ffb22
Ryan James <ryangregoryjames@gmail.com>
2018-01-19 01:17:38 -0800

    add tests for some inference code

---
+++ dit/inference/tests/test_binning.py
@@ -0,0 +1,26 @@
+"""
+Tests for dit.inference.binning.
+"""
+
+import numpy as np
+
+from dit.inference import binned
+
+
+def test_maxent_binning():
+    """
+    Test maxent binning.
+    """
+    data = np.random.random(10)
+    bd = binned(data)
+    assert sum(bd == 0) == 5
+
+
+def test_uniform_binning():
+    """
+    Test maxent binning.
+    """
+    data = np.random.random(10)
+    bins = np.array([0]*8 + [1]*2)
+    bd = binned(data+(2*bins), style='uniform')
+    assert np.all(bd == bins)
---
+++ dit/inference/tests/test_timeseries.py
@@ -0,0 +1,33 @@
+"""
+Tests for dit.inference.time_series.
+"""
+from __future__ import division
+
+import pytest
+
+from random import choice
+
+from dit import Distribution
+from dit.inference import dist_from_timeseries
+
+
+def golden_mean():
+    """
+    Generator of the golden mean process
+    """
+    val = choice([0, 1])
+    while True:
+        val = 0 if val == 1 else choice([0, 1])
+        yield val
+
+
+@pytest.mark.flaky(reruns=5)
+def test_dfts():
+    """
+    Test inferring a distribution from a time-series.
+    """
+    gm = golden_mean()
+    ts = [next(gm) for _ in range(1000000)]
+    d1 = dist_from_timeseries(ts)
+    d2 = Distribution([((0,), 0), ((0,), 1), ((1,), 0)], [1/3, 1/3, 1/3])
+    assert d1.is_approx_equal(d2, atol=1e-3)
