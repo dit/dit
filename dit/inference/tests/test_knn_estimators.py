dd35053cad8fcc6ba1e176d5b3f6260e61f259f9
Ryan James <ryangregoryjames@gmail.com>
2018-01-20 10:10:03 -0800

    more test tweaks

--- dit/inference/tests/test_knn_estimators.py
+++ dit/inference/tests/test_knn_estimators.py
@@ -41,7 +41,7 @@ def test_mi_knn(means, stds, rho):
     assert mi == pytest.approx(-np.log2(1-rho**2)/2, abs=1e-1)
 
 
-@settings(deadline=None, max_examples=25)
+@settings(deadline=None, max_examples=10)
 @given(means=lists(floats(min_value=-2.0, max_value=2.0), min_size=3, max_size=3),
        stds=lists(floats(min_value=0.1, max_value=2.0), min_size=3, max_size=3),
        rho=floats(min_value=-0.9, max_value=0.9))
@@ -52,7 +52,7 @@ def test_cmi_knn(means, stds, rho):
     cov = np.array([[stds[0]**2, stds[0]*stds[1]*rho, 0],
                     [stds[0]*stds[1]*rho, stds[1]**2, 0],
                     [0, 0, stds[2]**2]])
-    n = 100000
+    n = 150000
     data = np.random.multivariate_normal(means, cov, n)
     mi = total_correlation_ksg(data, [[0], [1]], [2])
-    assert mi == pytest.approx(-np.log2(1-rho**2)/2, abs=1e-1)
+    assert mi == pytest.approx(-np.log2(1-rho**2)/2, abs=1-1)
