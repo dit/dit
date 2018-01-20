65e8e7d1f3d5208331a8282b2b9e40f6d125430c
Ryan James <ryangregoryjames@gmail.com>
2018-01-20 09:39:22 -0800

    more test tweaks

--- dit/inference/tests/test_knn_estimators.py
+++ dit/inference/tests/test_knn_estimators.py
@@ -20,13 +20,13 @@ def test_entropy_knn(mean, std):
     """
     Test entropy of normal samples.
     """
-    n = 250000
+    n = 100000
     data = np.random.normal(mean, std, n).reshape(n, 1)
     h = differential_entropy_knn(data)
-    assert h == pytest.approx(np.log2(2*np.pi*np.e*std**2)/2, abs=5e-2)
+    assert h == pytest.approx(np.log2(2*np.pi*np.e*std**2)/2, abs=1e-1)
 
 
-@settings(deadline=None, timeout=unlimited, max_examples=25)
+@settings(deadline=None, max_examples=25)
 @given(means=lists(floats(min_value=-2.0, max_value=2.0), min_size=2, max_size=2),
        stds=lists(floats(min_value=0.1, max_value=2.0), min_size=2, max_size=2),
        rho=floats(min_value=-0.9, max_value=0.9))
@@ -35,13 +35,13 @@ def test_mi_knn(means, stds, rho):
     Test entropy of normal samples.
     """
     cov = np.array([[stds[0]**2, stds[0]*stds[1]*rho], [stds[0]*stds[1]*rho, stds[1]**2]])
-    n = 250000
+    n = 100000
     data = np.random.multivariate_normal(means, cov, n)
     mi = total_correlation_ksg(data, [[0], [1]])
-    assert mi == pytest.approx(-np.log2(1-rho**2)/2, abs=5e-2)
+    assert mi == pytest.approx(-np.log2(1-rho**2)/2, abs=1e-1)
 
 
-@settings(deadline=None, timeout=unlimited, max_examples=25)
+@settings(deadline=None, max_examples=25)
 @given(means=lists(floats(min_value=-2.0, max_value=2.0), min_size=3, max_size=3),
        stds=lists(floats(min_value=0.1, max_value=2.0), min_size=3, max_size=3),
        rho=floats(min_value=-0.9, max_value=0.9))
@@ -52,7 +52,7 @@ def test_cmi_knn(means, stds, rho):
     cov = np.array([[stds[0]**2, stds[0]*stds[1]*rho, 0],
                     [stds[0]*stds[1]*rho, stds[1]**2, 0],
                     [0, 0, stds[2]**2]])
-    n = 250000
+    n = 100000
     data = np.random.multivariate_normal(means, cov, n)
     mi = total_correlation_ksg(data, [[0], [1]], [2])
     assert mi == pytest.approx(-np.log2(1-rho**2)/2, abs=1e-1)
