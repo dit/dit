96069eb892641bab92715e353af443053ef54014
Ryan James <ryangregoryjames@gmail.com>
2018-02-26 13:39:25 -0800

    use reset_params()

--- dit/utils/tests/test_units.py
+++ dit/utils/tests/test_units.py
@@ -13,6 +13,7 @@ import numpy as np
 from dit import Distribution, ditParams
 from dit.algorithms.channelcapacity import channel_capacity_joint
 from dit.multivariate import entropy
+from dit.params import reset_params
 from dit.utils.units import ureg
 
 
@@ -23,7 +24,7 @@ def test_bit():
     d = Distribution(['0', '1'], [1/2, 1/2])
     ditParams['units'] = True
     h = entropy(d)
-    ditParams['units'] = False
+    reset_params()
     true = ureg.Quantity(1, ureg.bit)
     assert h == pytest.approx(true)
 
@@ -35,7 +36,7 @@ def test_nat():
     d = Distribution(['0', '1'], [1/2, 1/2])
     ditParams['units'] = True
     h = entropy(d)
-    ditParams['units'] = False
+    reset_params()
     true = ureg.Quantity(np.log(2), ureg.nat)
     assert h == pytest.approx(true)
 
@@ -47,7 +48,7 @@ def test_dit():
     d = Distribution(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], [1/10]*10)
     ditParams['units'] = True
     h = entropy(d)
-    ditParams['units'] = False
+    reset_params()
     true = ureg.Quantity(1, ureg.dit)
     assert h == pytest.approx(true)
 
@@ -59,6 +60,6 @@ def test_cc():
     gm = Distribution(['00', '01', '10'], [1/3]*3)
     ditParams['units'] = True
     cc, marg = channel_capacity_joint(gm, [0], [1], marginal=True)
-    ditParams['units'] = False
+    reset_params()
     true = ureg.Quantity(0.3219280796196524, ureg.bit)
     assert cc == pytest.approx(true)
