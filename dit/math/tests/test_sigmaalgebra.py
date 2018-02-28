44f4a01ac890a292811095d954f9ad9dcee32456
Ryan James <ryangregoryjames@gmail.com>
2018-02-27 16:03:13 -0800

    more tests

--- dit/math/tests/test_sigmaalgebra.py
+++ dit/math/tests/test_sigmaalgebra.py
@@ -3,7 +3,9 @@ Tests for dit.math.sigmaalgebra.
 """
 import pytest
 
-from dit.math.sigmaalgebra import is_sigma_algebra, is_sigma_algebra__brute, atom_set
+import numpy as np
+
+from dit.math.sigmaalgebra import sets2matrix, is_sigma_algebra, is_sigma_algebra__brute, atom_set
 
 
 sa = frozenset([frozenset([]), frozenset(['a']), frozenset(['b']), frozenset(['c']), 
@@ -12,6 +14,47 @@ sa = frozenset([frozenset([]), frozenset(['a']), frozenset(['b']), frozenset(['c
 not_sa1 = frozenset([frozenset([]), frozenset('c'), frozenset('b'), frozenset(['a', 'b'])])
 not_sa2 = frozenset([frozenset([]), frozenset(['a']), frozenset(['b']), frozenset(['c']), 
                 frozenset(['a', 'b']), frozenset(['a', 'c']), frozenset(['b', 'c'])])
+not_sa3 = frozenset([frozenset([]), frozenset('c'), frozenset(['a', 'b', 'c'])])
+
+
+def test_s2m1():
+    """
+    Test matrix construction.
+    """
+    m, _ = sets2matrix(sa)
+    true_m = set([(0, 1, 0),
+                  (0, 0, 1),
+                  (0, 0, 0),
+                  (1, 0, 1),
+                  (1, 0, 0),
+                  (1, 1, 1),
+                  (1, 1, 0),
+                  (0, 1, 1)])
+    assert set(map(tuple, m.tolist())) == true_m
+
+
+def test_s2m2():
+    """
+    Test matrix construction.
+    """
+    m, _ = sets2matrix(sa, X=['a', 'b', 'c'])
+    true_m = set([(0, 1, 0),
+                  (0, 0, 1),
+                  (0, 0, 0),
+                  (1, 0, 1),
+                  (1, 0, 0),
+                  (1, 1, 1),
+                  (1, 1, 0),
+                  (0, 1, 1)])
+    assert set(map(tuple, m.tolist())) == true_m
+
+
+def test_s2m3():
+    """
+    Test matrix construction.
+    """
+    with pytest.raises(Exception):
+        sets2matrix(sa, X=['a'])
 
 
 def test_isa1():
@@ -56,6 +99,13 @@ def test_isa6():
     Test not SA brute.
     """
     assert not is_sigma_algebra__brute(not_sa2, X=['a', 'b', 'c'])
+
+
+def test_isa7():
+    """
+    Test not SA brute.
+    """
+    assert not is_sigma_algebra__brute(not_sa3)
 
 
 def test_atom_set1():
