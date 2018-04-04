fb414b472c63fa80a602ce378cd002947f5f4c99
Ryan James <ryangregoryjames@gmail.com>
2018-04-03 17:25:25 -0700

    sum over crvs too!

--- dit/rate_distortion/curves.py
+++ dit/rate_distortion/curves.py
@@ -164,8 +164,8 @@ class IBCurve(object):
             entropies.append(ib.entropy(pmf))
             relevances.append(ib.relevance(pmf))
             errors.append(ib.error(pmf))
-            ranks.append(np.linalg.matrix_rank(pmf.sum(axis=1)))
-            alphabets.append((pmf.sum(axis=(0, 1)) > 0).sum())
+            ranks.append(np.linalg.matrix_rank(pmf.sum(axis=(1, 2))))
+            alphabets.append((pmf.sum(axis=(0, 1, 2)) > 1e-6).sum())
 
         print(ranks)
         print(alphabets)
