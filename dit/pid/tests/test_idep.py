8a09b49c1a7d11d2ac0b1f12e3efbacb54ad22dc
Ryan James <ryangregoryjames@gmail.com>
2018-11-01 16:40:30 -0700

    add more tests

--- dit/pid/tests/test_idep.py
+++ dit/pid/tests/test_idep.py
@@ -67,3 +67,14 @@ def test_pid_ra1():
     assert pid[((0,),)] == pytest.approx(0.5, abs=1e-4)
     assert pid[((1,),)] == pytest.approx(0.5, abs=1e-4)
     assert pid[((0, 1),)] == pytest.approx(0.0, abs=1e-4)
+
+def test_pid_ra2():
+    """
+    """
+    d = trivariates['cat']
+    pid = PID_RA(d, ((0,), (1,), (2,)), (3,))
+    for atom in pid._lattice:
+        if atom in (((0,),), ((1,),), ((2,),)):
+            assert pid[atom] == pytest.approx(1.0, abs=1e-4)
+        else:
+            assert pid[atom] == pytest.approx(0.0, abs=1e-4)
