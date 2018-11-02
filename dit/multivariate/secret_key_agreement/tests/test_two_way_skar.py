fa00b11e749a08db465df749ac37057296bd0365
Ryan James <ryangregoryjames@gmail.com>
2018-11-01 17:15:19 -0700

    add a test

---
+++ dit/multivariate/secret_key_agreement/tests/test_two_way_skar.py
@@ -0,0 +1,28 @@
+"""
+Tests for dit.multivariate.secret_key_agreement.two_way_skar.py
+"""
+from __future__ import division
+
+import pytest
+
+import numpy as np
+
+from dit import Distribution
+from dit.example_dists.intrinsic import intrinsic_1
+from dit.multivariate.secret_key_agreement import two_way_skar
+
+
+def test_two_way_skar1():
+    """
+    Test simple example 1.
+    """
+    skar = two_way_skar(intrinsic_1, [[0], [1]], [2])
+    assert skar == pytest.approx(0.0)
+
+def test_two_way_skar2():
+    """
+    Test an unknown example.
+    """
+    d = Distribution(['000', '011', '020', '101'], [1/4]*4)
+    skar = two_way_skar(d, [[0], [2]], [1])
+    assert np.isnan(skar)
