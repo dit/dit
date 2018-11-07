f6194a10d661cdcff90339c4d65b9be1dc723ef5
Ryan James <ryangregoryjames@gmail.com>
2018-11-07 15:37:08 -0800

    faster tests

--- dit/multivariate/secret_key_agreement/tests/test_two_part_intrinsic_mutual_information.py
+++ dit/multivariate/secret_key_agreement/tests/test_two_part_intrinsic_mutual_information.py
@@ -4,7 +4,7 @@ Tests for dit.multivariate.secret_key_agreement.two_part_intrinsic_mutual_inform
 
 import pytest
 
-from dit.example_dists.intrinsic import intrinsic_1, intrinsic_2, intrinsic_3
+from dit.example_dists import giant_bit, n_mod_m
 from dit.multivariate.secret_key_agreement import (
     two_part_intrinsic_total_correlation,
     two_part_intrinsic_dual_total_correlation,
@@ -12,25 +12,34 @@ from dit.multivariate.secret_key_agreement import (
 )
 
 
-@pytest.mark.parametrize('dist', [intrinsic_1, intrinsic_2, intrinsic_3])
-def test_tpitc1(dist):
+@pytest.mark.parametrize(['dist', 'value'], [
+    (giant_bit(3, 2), 0.0),
+    (n_mod_m(3, 2), 0.0)
+])
+def test_tpitc1(dist, value):
     """
     """
     tpitc = two_part_intrinsic_total_correlation(dist, [[0], [1]], [2])
-    assert tpitc == pytest.approx(dist.secret_rate)
+    assert tpitc == pytest.approx(value)
 
 
-@pytest.mark.parametrize('dist', [intrinsic_1, intrinsic_2, intrinsic_3])
-def test_tpidtc1(dist):
+@pytest.mark.parametrize(['dist', 'value'], [
+    (giant_bit(3, 2), 0.0),
+    (n_mod_m(3, 2), 0.0)
+])
+def test_tpidtc1(dist, value):
     """
     """
     tpidtc = two_part_intrinsic_dual_total_correlation(dist, [[0], [1]], [2])
-    assert tpidtc == pytest.approx(dist.secret_rate)
+    assert tpidtc == pytest.approx(value)
 
 
-@pytest.mark.parametrize('dist', [intrinsic_1, intrinsic_2, intrinsic_3])
-def test_tpicaekl1(dist):
+@pytest.mark.parametrize(['dist', 'value'], [
+    (giant_bit(3, 2), 0.0),
+    (n_mod_m(3, 2), 0.0)
+])
+def test_tpicaekl1(dist, value):
     """
     """
     tpicaekl = two_part_intrinsic_CAEKL_mutual_information(dist, [[0], [1]], [2])
-    assert tpicaekl == pytest.approx(dist.secret_rate)
+    assert tpicaekl == pytest.approx(value)
