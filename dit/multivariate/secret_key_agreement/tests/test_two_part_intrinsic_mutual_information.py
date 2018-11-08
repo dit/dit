a49c6a52272adf815d63b98e19010ed4e5e2b90d
Ryan James <ryangregoryjames@gmail.com>
2018-11-07 16:29:02 -0800

    add bounds to tests

--- dit/multivariate/secret_key_agreement/tests/test_two_part_intrinsic_mutual_information.py
+++ dit/multivariate/secret_key_agreement/tests/test_two_part_intrinsic_mutual_information.py
@@ -19,7 +19,7 @@ from dit.multivariate.secret_key_agreement import (
 def test_tpitc1(dist, value):
     """
     """
-    tpitc = two_part_intrinsic_total_correlation(dist, [[0], [1]], [2])
+    tpitc = two_part_intrinsic_total_correlation(dist, [[0], [1]], [2], bound_j=2, bound_u=2, bound_v=2)
     assert tpitc == pytest.approx(value)
 
 
@@ -30,7 +30,7 @@ def test_tpitc1(dist, value):
 def test_tpidtc1(dist, value):
     """
     """
-    tpidtc = two_part_intrinsic_dual_total_correlation(dist, [[0], [1]], [2])
+    tpidtc = two_part_intrinsic_dual_total_correlation(dist, [[0], [1]], [2], bound_j=2, bound_u=2, bound_v=2)
     assert tpidtc == pytest.approx(value)
 
 
@@ -41,5 +41,5 @@ def test_tpidtc1(dist, value):
 def test_tpicaekl1(dist, value):
     """
     """
-    tpicaekl = two_part_intrinsic_CAEKL_mutual_information(dist, [[0], [1]], [2])
+    tpicaekl = two_part_intrinsic_CAEKL_mutual_information(dist, [[0], [1]], [2], bound_j=2, bound_u=2, bound_v=2)
     assert tpicaekl == pytest.approx(value)
