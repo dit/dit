13e25b2db5841eb079835e140ac79ed257fcfa96
Ryan G. James <ryangregoryjames@gmail.com>
2020-01-11 15:40:37 -0800

    add kochinski measure

---
+++ dit/pid/measures/__init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""
+All the measures.
+"""
+
+from .ibroja import PID_BROJA
+from .iccs import PID_CCS
+from .idep import PID_dep, PID_RA
+from .imin import PID_WB
+from .immi import PID_MMI
+from .ipm import PID_PM
+from .ipreceq import PID_Preceq
+from .iproj import PID_Proj
+from .irav import PID_RAV
+from .irr import PID_RR
+from .iskar import PID_SKAR_owb
+from .iwedge import PID_GK
+
+
+__all_pids = [
+    PID_MMI,
+    PID_GK,
+    PID_WB,
+    PID_RR,
+    PID_CCS,
+    PID_PM,
+    PID_Proj,
+    PID_BROJA,
+    PID_dep,
+    PID_RA,
+    PID_RAV,
+    PID_SKAR_owb,
+    PID_Preceq,
+]
