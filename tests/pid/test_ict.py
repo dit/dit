ff74c94cdadac0572a109a7580607b17796e3428
Ryan G. James <ryangregoryjames@gmail.com>
2020-02-06 23:37:15 -0800

    add tests

---
+++ tests/pid/test_ict.py
@@ -0,0 +1,20 @@
+"""
+Tests for dit.pid.measures.ict.
+"""
+
+import pytest
+
+from dit.pid.measures.ict import PID_CT
+from dit.pid.distributions import bivariates
+
+
+def test_pid_ct1():
+    """
+    Test ict on a generic distribution.
+    """
+    d = bivariates['reduced or']
+    pid = PID_CT(d, ((0,), (1,)), (2,))
+    assert pid[((0,), (1,))] == pytest.approx(0.02712, abs=1e-4)
+    assert pid[((0,),)] == pytest.approx(0.28416, abs=1e-4)
+    assert pid[((1,),)] == pytest.approx(0.28416, abs=1e-4)
+    assert pid[((0, 1),)] == pytest.approx(0.40456, abs=1e-4)
