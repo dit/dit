cb40a78be6c2f48f21d3834b8a25bf78c0c93285
Ryan James <ryangregoryjames@gmail.com>
2022-07-20 23:03:39 -0600

    🔀 Stochastic gk (#197)
    * add the implementation
    
    * try to rerun any single failure
    
    Co-authored-by: Ryan James <ryan.james@reddit.com>

--- dit/multivariate/common_informations/__init__.py
+++ dit/multivariate/common_informations/__init__.py
@@ -6,4 +6,5 @@ from .exact_common_information import exact_common_information
 from .functional_common_information import functional_common_information
 from .gk_common_information import gk_common_information
 from .mss_common_information import mss_common_information
+from .stochastic_gk_common_information import stochastic_gk_common_information
 from .wyner_common_information import wyner_common_information
