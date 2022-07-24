8c4d6b2612f050bb69bbc9c228c5e908bb649857
Ryan James <ryan.james@reddit.com>
2022-07-24 15:27:56 -0600

    start to allow bound on i_prec

--- dit/pid/measures/iprec.py
+++ dit/pid/measures/iprec.py
@@ -45,7 +45,7 @@ class PID_Prec(BasePID):
     _name = "I_≺"
 
     @staticmethod
-    def get_solution_information(d, sources, target):
+    def get_solution_information(d, sources, target, n_q=None):
         """
         Compute I_prec(inputs : output) =
             \\max I[Q : output] such that Q \\prec X_i
@@ -59,6 +59,12 @@ class PID_Prec(BasePID):
             The source variables.
         target : iterable
             The target variable.
+        n_q : int (default None)
+            The cardinality of the redundancy random variable Q. If not specified,
+            then use the cardinality from Theorem A1 in the paper (this cardinality
+            is always sufficient). Choosing a lower cardinality by setting n_q can
+            dramatically speed up computation, but at the cost of only providing a
+            lower bound on the Blackwell redundancy
 
         Returns
         -------
@@ -76,8 +82,11 @@ class PID_Prec(BasePID):
         probs_y = np.array([pY[y] for y in pjoint.alphabet[target_rvndx]])
         n_y = len(pY)
 
-        if n_y <= 1:
-            # Trivial case where target has a single outcome, redundancy has to be 0
+        if not (n_q is None or (isinstance(n_q, int) and n_q >= 1)):
+            raise Exception('Parameter n_q should be None or positive integer')
+
+        if n_y <= 1 or n_q == 1:
+            # Trivial case where target or redundancy has a single outcome, redundancy has to be 0
             return 0, {}
 
         # variablesQgiven holds ppl variables that represent conditional probability
@@ -87,10 +96,10 @@ class PID_Prec(BasePID):
 
         var_ix = 0  # counter for tracking how many variables we've created
 
-
-        # Calculate the maximum number of outcomes we will require for Q
-        n_q = sum([len(alphabet) - 1 for rvndx, alphabet in enumerate(pjoint.alphabet)
-                                 if rvndx != target_rvndx]) + 1
+        if n_q is None:
+            # Calculate the maximum number of outcomes we will require for Q, per Theorem A1
+            n_q = sum([len(alphabet) - 1 for rvndx, alphabet in enumerate(pjoint.alphabet)
+                                    if rvndx != target_rvndx]) + 1
 
         # Iterate over all the random variables (R.V.s): i.e., all the sources + the target
         for rvndx, rv in enumerate(pjoint.rvs):
@@ -144,7 +153,7 @@ class PID_Prec(BasePID):
             for q in range(n_q):
                 for y_ix, y in enumerate(pjoint.alphabet[target_rvndx]):
                     z = np.zeros(num_vars)
-                    cur_marg = 0. 
+                    cur_marg = 0.
                     for x_ix, x in enumerate(pjoint.alphabet[rvndx]):
                         pXY = pYSource[pYSource._outcome_ctor((x,y))]
                         cur_marg += pXY
@@ -166,7 +175,7 @@ class PID_Prec(BasePID):
         def objective(x):  # efficient calculation of mutual information
             # Map solution vector x to joint distribution over Q and Y
             pQY  = x.dot(mul_mx).reshape((n_q,n_y))
-            pQY += 1e-12   # Remove small negative values that may appear due to numerical issues 
+            pQY += 1e-12   # Remove small negative values that may appear due to numerical issues
             probs_q = pQY.sum(axis=1)
             H_YgQ = entr(pQY / probs_q[:, None]).sum(axis=1).dot(probs_q)
             v = (H_Y - H_YgQ)/ln2
@@ -178,7 +187,7 @@ class PID_Prec(BasePID):
         # set of extreme points of the corresponding polytope. It then calls
         # objective on each extreme point
         x_opt, v_opt = maximize_convex_function(f=objective,
-            A_eq=np.array(A_eq)    , b_eq=np.array(b_eq), 
+            A_eq=np.array(A_eq)    , b_eq=np.array(b_eq),
             A_ineq=np.array(A_ineq), b_ineq=np.array(b_ineq))
 
         # Return mutual information I(Q;Y) and solution information
@@ -196,13 +205,13 @@ class PID_Prec(BasePID):
         return v_opt, sol
 
     @classmethod
-    def _measure(cls, d, sources, target):
+    def _measure(cls, d, sources, target, n_q=None):
         """
         See information for get_solution_information method.
         """
         if len(sources) == 1:
             return mutual_information(d, sources[0], target)
-        v_opt, sol = cls.get_solution_information(d, sources, target)
+        v_opt, _ = cls.get_solution_information(d, sources, target, n_q)
         return v_opt
 
 
